9a7c68ad5ca877989a2e2624d37db2b1aabe469b
yxxjxxxhxxx <whdgus3012@gmail.com>
2022-08-23 23:52:50 +0900

    22823

---
+++ MainTest.java
@@ -0,0 +1,8 @@
+public class MainTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ algorithm/Step01/Step01_005.java
@@ -0,0 +1,15 @@
+package algorithm.Step01;
+import java.util.Scanner;
+public class Step01_005 {
+		public static void main(String[] args) {
+	 
+			Scanner scanner = new Scanner(System.in);
+			int A = scanner.nextInt();
+			int B = scanner.nextInt();
+			
+			System.out.println(A+B);
+	 
+			scanner.close();
+		}	
+}
+
