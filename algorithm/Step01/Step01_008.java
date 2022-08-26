53ccbe7c867ae839f6bf7aa3ddd21ccd9d9882ce
BaBo coder <103405475+yxxjxxxhxxx@users.noreply.github.com>
2022-08-26 23:27:16 +0900

    solve

---
+++ algorithm/Step01/Step01_008.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Main {
+    public static void main (String[] args) {
+        Scanner in = new Scanner(System.in);
+        double a = in.nextDouble();
+        double b = in.nextDouble();
+        System.out.print(a / b);
+        in.close();
+    }
+
+}
